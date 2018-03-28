fbfb6a847bbd8f24fbf54976496b18817a8a23d7
Mehmood Ahmad <maksuperlink@gmail.com>
2018-03-28 06:34:31 +0500

    Random Quote - Layout Complete

--- 9- Intermediate Front End Projects/Build a Random Quote Machine/gulpfile.js
+++ 9- Intermediate Front End Projects/Build a Random Quote Machine/gulpfile.js
@@ -1,18 +1,32 @@
 'use strict';
 
 const gulp = require('gulp');
-var htmlmin = require('gulp-htmlmin');
+const gulpif = require('gulp-if');
+const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const minifyjs = require('gulp-js-minify');
-var browserSync = require('browser-sync').create();
+const clean = require('gulp-clean');
+const browserSync = require('browser-sync').create();
+
+let environment = 'development';
+let location = './build/';
+
+if(environment === 'production'){
+  location = './dist/'
+}
+
+gulp.task('clean', function() {
+  return gulp.src(location+ '**/*.*')
+      .pipe(clean());
+});
 
 gulp.task('html', function() {
   return gulp.src('./src/*.html')
-      .pipe(htmlmin({collapseWhitespace: true}))
-      .pipe(gulp.dest('./build/'))
+      .pipe(gulpif( environment === 'production', htmlmin({collapseWhitespace: true})))
+      .pipe(gulp.dest(location))
       .pipe(browserSync.stream());
 });
 
@@ -22,31 +36,32 @@ gulp.task('js', function () {
       .pipe(babel({
         presets: ['env']
       }))
-      .pipe(concat('bundle.js'))
-      .pipe(minifyjs())
+      .pipe(gulpif( environment === 'production', concat('bundle.min.js')))
+      .pipe(gulpif( environment === 'production', minifyjs()))
       .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest('./build/js'))
+      .pipe(gulp.dest(location+ 'js'))
       .pipe(browserSync.stream());
 });
 
 gulp.task('plugins', function () {
   gulp.src(["./src/plugins/**/*.*"], {base: "./src/plugins"})
-      .pipe(gulp.dest("./build/plugins"));
+      .pipe(gulp.dest(location+ "plugins"));
 })
 
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
       .pipe(sourcemaps.init())
-      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulpif( environment === 'production', sass({outputStyle: 'compressed'}).on('error', sass.logError)))
       .pipe(sourcemaps.write())
-      .pipe(gulp.dest('./build/css'))
+      .pipe(gulp.dest(location+ 'css'))
       .pipe(browserSync.stream());
 });
 
 gulp.task('serve', function() {
   browserSync.init({
     server: {
-      baseDir: "./build/"
+      baseDir: location
     }
   });
 });
@@ -60,4 +75,4 @@ gulp.watch('./src/plugins/**/*.*', ['plugins']);
 
 // ========================= Default ===================
 
-gulp.task('default', ['html', 'sass', 'js', 'plugins','serve']);
+gulp.task('default', ['clean','html', 'sass', 'js', 'plugins','serve']);
--- 9- Intermediate Front End Projects/Build a Random Quote Machine/src/js/main.js
+++ 9- Intermediate Front End Projects/Build a Random Quote Machine/src/js/main.js
@@ -1,5 +1,11 @@
-var nameBuilder = function(firstName="Johns", lastName="Doe") {
+"use strict";
+
+var nameBuilder = function nameBuilder() {
+  var firstName = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : "Johns";
+  var lastName = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "Doe";
+
   console.log(firstName + " " + lastName);
 };
 
 nameBuilder();
+//# sourceMappingURL=main.js.map
