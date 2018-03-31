06d7bfc48e8e44248194789c3897821f1c1234f4
null <maksuperlink@gmail.com>
2018-03-31 12:28:24 +0500

    Twitch API - api implemented

--- 9- Intermediate Front End Projects/Twitch JSON API/src/js/main.js
+++ 9- Intermediate Front End Projects/Twitch JSON API/src/js/main.js
@@ -1,24 +1,11 @@
 "use strict";
 
-let channels = ['one', 'two', 'three', 'four', 'five', 'six']
+//channels list to search in twitch
+let channels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
 
-function showChannels(){
-  let filter = $('.search-filters .radio.active').attr('data-target');
-  console.log('Current Filter: ' + filter);
-
-  if(filter === 'all'){
-    $('#twitch-channels .channel-item-wrapper').show();
-  }else{
-    $('#twitch-channels .channel-item-wrapper').hide();
-    $('#twitch-channels .channel-item-wrapper[data-status=' + filter + ']').show();
-  }
-}
-function showFilteredChannels(string){
-  let filtered = channels.filter(channel => channel.indexOf(string) >= 0 );
-
-  $('.channel-item-wrapper').hide();
-  filtered.map( channel => {
-    $('.channel-item-wrapper[data-channel=' + channel+ ']').show();
+function capitalize(string) {
+  return string.replace(/\b\w/g, function (l) {
+    return l.toUpperCase()
   })
 }
 
@@ -28,58 +15,128 @@ function showLoading() {
 function hideLoading() {
   $('.loading').hide();
 }
+//show channels based on search value of radio button
+function showChannels(search_query) {
+  $('#twitch-channels .channel-item-wrapper').hide();
+  let status = $('.search-filters .radio.active').attr('data-target');
 
-function getData(value) {
-  let encoded = encodeURI(value);
-  let url = `https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${encoded}`;
+  if (search_query) {
+    let filtered = channels.filter(channel => channel.toLowerCase().indexOf(search_query) >= 0);
+
+    console.log(filtered);
+    filtered.map(channel => {
+      if (status !== 'all') { // use status if given
+        $('.channel-item-wrapper[data-channel=' + channel.toLowerCase() + '][data-status=' + status + ']').show();
+      } else {
+        $('.channel-item-wrapper[data-channel=' + channel.toLowerCase() + ']').show();
+      }
+    })
+  } else {
+    if (status !== 'all') { // use status if given
+      $('.channel-item-wrapper[data-status=' + status + ']').show();
+    } else {
+      $('.channel-item-wrapper').show();
+    }
+  }
+}
+
+//get data from all channels
+function getChannelsData() {
+  channels.filter(channel => getStreamData(channel));
+}
+//get stream data (no logo if not streaming)
+function getStreamData(channelID) {
+  let encoded = encodeURI(channelID);
+  let url = `https://wind-bow.glitch.me/twitch-api/streams/${encoded}`;
   $.ajax({
     method: 'GET',
     url: url,
     dataType: 'jsonp',
     crossDomain: true,
     beforeSend: function (e) {
-      $('.articles-wrapper').remove();
       showLoading();
     }
   })
 
       .done(function (data) {
-        showData(data);
+        showStreamData(data, channelID);
+
+        if (!data.stream) { //extra call for getting the logo of the channel
+          getChannelData(channelID);
+        }
+        // console.log('Channel Stream : ' + channelID);
+        // console.log(data);
       })
       .fail(function (data) {
-        console.log(data);
-        showNavigationError()
+        console.error(data);
+        showAjaxError();
       })
       .always(function () {
         hideLoading();
       })
 
 }
-function showData(data) {
-  let pages = data['query']['pages'];
-  console.log('Pages Recieved');
-  console.log(pages);
+//get channels data  (for getting logo if not streaming)
+function getChannelData(channelID) {
+  let encoded = encodeURI(channelID);
+  let url = `https://wind-bow.glitch.me/twitch-api/channels/${encoded}`;
+  $.ajax({
+    method: 'GET',
+    url: url,
+    dataType: 'jsonp',
+    crossDomain: true,
+    beforeSend: function (e) {
+    }
+  })
 
-  $('.search-wrapper').after('<div class="articles-wrapper"></div>');
+      .done(function (data) {
+        showChannelData(data, channelID.toLowerCase());
+        // console.log('Channels Data : ');
+        // console.log(data);
+      })
+      .fail(function (data) {
+        console.error(data);
+      })
+      .always(function () {
+      })
 
-  for (let page in pages) {
-    let title = pages[page]['title'];
-    let text = pages[page]['extract'];
+}
+//show stream data of channels
+function showStreamData(data, channelID) {
+  let layout, status, description, logo;
 
-    let layout = '<div class="article-item-wrapper"> ' +
-        '<a class="article-item" href="https://en.wikipedia.org/?curid='+ page + '" target="_blank"> ' +
-        '<h2 class="article-title"> ' + title +' </h2> ' +
-        '<p class="article-text"> ' + text + ' </p>' +
-        '</a> ' +
-        '</div>';
-
-    $('.articles-wrapper').append(layout);
+  if (data.stream) {
+    status = 'online';
+    description = data.stream.channel.status;
+    logo = data.stream.channel.logo;
+  } else {
+    status = 'offline';
+    description = '';
+    logo = 'http://via.placeholder.com/50x50';
   }
 
-  $('.viewer-container').animate({marginTop: '40px'}, 300);
-  $('.articles-wrapper').addClass('animated bounceInUp').css('display', 'block');
+  layout = `<div class="channel-item-wrapper col-lg-4 col-md-6 col-sm-6 col-xs-12" data-channel="${channelID.toLowerCase()}" data-status="${status}">
+      <a class="channel-item" href="https://www.twitch.tv/${channelID}" target="_blank">
+      <div class="channel-img">
+      <img src="${logo}" alt="channel logo">
+      </div>
+      <div class="channel-info">
+      <div class="channel-name"> ${channelID}</div>
+  <div class="channel-status ${status}"> ${capitalize(status)}</div>
+      <div class="channel-description"> ${description}</div>
+  </div>
+  </a>
+  </div>`;
+
+  $('#twitch-channels').append(layout);
 }
-function showNavigationError(msg = 'An error has been occured. Please Try Again') {
+//show channel data (just logo)
+function showChannelData(data, channelID) {
+  let logo = data.logo;
+  $('.channel-item-wrapper[data-channel=' + channelID + '] .channel-img > img').attr('src', logo);
+}
+//show error
+function showAjaxError(msg = 'An error has been occured. Please Try Again') {
   $('#error-wrapper').html(`<div class="channel-error">${msg}</div>`);
 
   $('.viewer-container').animate({marginTop: '40px'}, 300);
@@ -115,14 +172,17 @@ $(document).ready(function (e) {
   //submitting form
   $('#search-channel').on('keyup', function (e) {
     let value = $(this).val()
-    showFilteredChannels(value);
+    showChannels(value.toLowerCase());
     console.log('Search Value: ' + value);
     event.preventDefault();
   })
 
-  //submitting form
+  //submitting form (preventing submission)
   $('#search-form').on('submit', function (e) {
     event.preventDefault();
   })
+
+  //loading channels on start
+  getChannelsData();
 
 });
