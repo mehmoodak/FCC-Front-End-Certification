70ebc121ff95acd424d6cf8d40ccd03dc184a6d5
null <maksuperlink@gmail.com>
2018-04-04 00:03:12 +0500

    Pomodoro Clock - Layout using basic HTML and CSS

--- 12- Advanced Front End Projects/Pomodoro Clock/src/js/main.js
+++ 12- Advanced Front End Projects/Pomodoro Clock/src/js/main.js
@@ -1,5 +1,15 @@
 "use strict";
 
+var bar = new ProgressBar.Circle('#clock-timer', {
+  strokeWidth: 4,
+  duration: 10,
+  color: '#00b1cc',
+  trailColor: '#fff',
+  trailWidth: 4,
+});
+
+bar.animate(0.8);  // Number from 0.0 to 1.0
+
 $(document).ready(function (e) {
 
 });
